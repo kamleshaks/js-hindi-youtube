f9ede875045f5f2e2bc37fb84b2d79e4539b5985
kamleshaks <37577662+kamleshaks@users.noreply.github.com>
2023-10-18 09:10:13 +0000

    varibales changes

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -5,19 +5,19 @@ accountCity = "Jaipur";
 let accountState;
 
 
+// accountId = 2; //not allowed
+
+
 accountEmail = "hdfc@hotmail.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
-
-
-// accountId = 2; //not allowed
+//console.log(accountId);
 
 /* Js New tips
 prefer not to use Var
 Because of issue in block scope and functional scope
 */
 
-//console.log(accountId);
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
