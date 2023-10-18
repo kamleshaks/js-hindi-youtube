49d0269b4f8f423c12c505fd0bb5c11b9a34f939
kamleshaks <37577662+kamleshaks@users.noreply.github.com>
2023-10-18 09:07:51 +0000

    variable changes

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -3,9 +3,17 @@ let accountEmail = "test@gmail.com";
 var accountPassword = "123454";
 accountCity = "Jaipur";
 let accountState;
+
+
+accountEmail = "hdfc@hotmail.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+
+
 // accountId = 2; //not allowed
 
-/*
+/* Js New tips
 prefer not to use Var
 Because of issue in block scope and functional scope
 */
